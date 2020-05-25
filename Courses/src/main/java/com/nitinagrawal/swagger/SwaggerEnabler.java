6d4b9b83ebaa50bc0efa1bf93682a5d90f33890d
nitinbly84 <nitin.bly@gmail.com>
2020-05-25 11:38:04 +0530

    SwaggerEnabler to enable Swagger, it was missed in earlier checkin

---
+++ Courses/src/main/java/com/nitinagrawal/swagger/SwaggerEnabler.java
@@ -0,0 +1,11 @@
+package com.nitinagrawal.swagger;
+
+import org.springframework.stereotype.Service;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Service
+@EnableSwagger2
+public class SwaggerEnabler {
+
+}
